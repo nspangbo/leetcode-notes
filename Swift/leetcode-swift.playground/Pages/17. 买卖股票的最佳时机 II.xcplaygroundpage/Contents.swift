aa93987a7400aa70cd57c08f1482d964d03cd3ca
nspangbo <nspangbo@gmail.com>
2019-06-26 19:14:21 +0800

    update [17. 买卖股票的最佳时机 II.swift]

--- Swift/leetcode-swift.playground/Pages/17. 买卖股票的最佳时机 II.xcplaygroundpage/Contents.swift
+++ Swift/leetcode-swift.playground/Pages/17. 买卖股票的最佳时机 II.xcplaygroundpage/Contents.swift
@@ -51,7 +51,9 @@ class Solution {
 }
 
 // test
-Solution.init(maxProfit([7,1,5,3,6,4]))
+Solution.init().maxProfit([7,1,5,3,6,4])
+Solution.init().maxProfit([1,2,3,4,5])
+Solution.init().maxProfit([7,6,4,3,1])
 
 
 //: [Next](@next)
